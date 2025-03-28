a12d19369601fe077742e4b108ee0f3cd9d77acf
Pedro Escapalete <pedroescapalete@gmail.com>
2025-03-28 16:23:48 -0300

    adiciona divisão de valor entre 4 pessoas

---
+++ Operadores_Aritmeticos/3_quadrado_e_cubo.js
@@ -0,0 +1,5 @@
+let numero = 9
+let quadrado = numero * numero
+let cubo = numero * numero *numero 
+
+console.log(`O ${numero} ao quadrado é ${quadrado} e ao cubo ${cubo} `);
---
+++ Operadores_Aritmeticos/4_divisão_de_dinheiro.js
@@ -0,0 +1,4 @@
+let dinheiro = 200
+let valor = dinheiro/5
+
+console.log('cada um recebera ' + valor);
