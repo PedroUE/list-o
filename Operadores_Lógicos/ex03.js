6115e14ca9d4983a96e09b2903bc41e05b0b0bfe
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 14:48:53 -0300

    adiciona verificação de número maior que 100

---
+++ Operadores_Lógicos/ex03.js
@@ -0,0 +1,9 @@
+let valor1 = 25;
+let valor2 = 25;
+let valor3 = 51;
+
+if(valor1 >= 100 || valor2 >= 100 || valor3 >= 100){
+    console.log(`Passa de cem`);
+} else{
+    console.log(`Não passa de cem`);
+}
