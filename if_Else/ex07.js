9c738e500550677c6d0ec7c0e95244c19bb382c7
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 20:54:36 -0300

    adiciona cálculo de desconto baseado em salário

---
+++ if_Else/ex07.js
@@ -0,0 +1,11 @@
+let salario = 2000;
+let descontinho;
+
+if(salario >= 2000){
+    console.log(`Foi descontado ${descontinho = salario * 0.10} de seu salario`)
+} else{
+    console.log(`Foi descontado 5% ${descontinho = salario * 0.05} de seu salario`)
+}
+
+let salarioFinal = salario - descontinho
+console.log(`Salario final ${salarioFinal}`)
