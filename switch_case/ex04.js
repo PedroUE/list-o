8686a9838e7776e6c77b0a5d9dd9188bcded928a
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 15:23:21 -0300

    adiciona atribuição de conceito por nota

---
+++ switch_case/ex04.js
@@ -0,0 +1,21 @@
+let nota = 9
+
+switch(true){
+     case nota >= 9 : console.log(`A`);
+     break;
+
+     case nota >= 7 : console.log(`B`);
+     break;
+
+     case nota >= 5 : console.log(`C`);
+     break;
+
+     case nota >= 3 : console.log(`D`);
+     break;
+
+     case nota >= 1: console.log(`E`);
+     break;
+
+     default: console.log(`Nota invalida.`)
+
+    } 
