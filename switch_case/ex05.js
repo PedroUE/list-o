84f4f350213ebffedb6796e69e3ec8fc0c93da92
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 15:27:17 -0300

    adiciona exibição da estação do ano

---
+++ switch_case/ex05.js
@@ -0,0 +1,17 @@
+let EstaçaoDoAno = 2;
+
+switch(EstaçaoDoAno){
+    case 1 : console.log(`Verão`);
+    break;
+
+    case 2 : console.log(`Outono`);
+    break;
+
+    case 3 : console.log(`Inverno`);
+    break; 
+
+    case 4: console.log(`Prima-Vera`);
+    break;
+
+    default: (`Estação Invalida.`)
+}
