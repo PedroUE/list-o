206e595fd343a26298014d8af5bf19dc6207c2c5
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 20:41:36 -0300

    adiciona verificação de divisibilidade por 3 e 5

---
+++ if_Else/ex04.js
@@ -0,0 +1,7 @@
+let valor = 300;
+
+if (valor % 3 === 0 && valor % 5 === 0){
+    console.log(`O numero é divisivel por 3 e 5!`);
+} else {
+    console.log(`o numero não é divisivel por 3 ou 5!`);
+}
