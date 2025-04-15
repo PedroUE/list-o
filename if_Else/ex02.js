7c686b8504d92c01f7e35d5500590d15222cb308
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 20:32:27 -0300

    adiciona comparação do maior entre dois números

---
+++ if_Else/ex02.js
@@ -0,0 +1,8 @@
+let valor1 = 4;
+let valor2 = 2;
+
+if(valor1 > valor2 ){
+    console.log(`O numero ${valor1} é maior que o ${valor2}`);
+} else{
+    console.log(`O numero ${valor2} é maior que o ${valor1}`);
+}
