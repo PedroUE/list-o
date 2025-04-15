523cab52befa61e8d324f0cd9d03087ea172b962
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 14:44:30 -0300

    idade para vootar

---
+++ Operadores_Lógicos/ex02.js
@@ -0,0 +1,13 @@
+let idade = 2;
+
+switch(idade){
+    case idade >= 70: console.log(`Pode votar`);
+        break;
+
+    case idade < 16: console.log(`Não pode votar`);
+    break;
+    
+    default: console.log(`Não pode votar`);
+    break;
+
+}   
