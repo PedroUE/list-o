96b27df66ac5e361310ac4f67d952131866aee85
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 20:38:35 -0300

    adiciona classificação etária por idade

---
+++ if_Else/ex03.js
@@ -0,0 +1,13 @@
+let idade = 32;
+
+if(idade <= 12){
+    console.log(`É criança`);
+} else if(idade <= 17){
+    console.log(`É Adolecente`);
+} else if(idade <= 59){
+    console.log(`É Adulto`);
+} else if(idade >= 60){
+    console.log(`É Idoso`);
+} else{
+    console.log(`Idade Invalida!`)
+}
