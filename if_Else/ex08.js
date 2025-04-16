5ad953a1e3b8a262a0a0b226c226136816508caf
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 21:01:47 -0300

    adiciona identificação do menor entre três números

---
+++ if_Else/ex08.js
@@ -0,0 +1,11 @@
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
+
+if (numero1 < numero2 & numero3){
+    console.log(`O ${numero1} é o menor`);
+} else if(numero2 < numero3 & numero1){
+    console.log(`O ${numero2} é o menor`);
+} else {
+    console.log(`O ${numero3} é o menor deles`);
+}
