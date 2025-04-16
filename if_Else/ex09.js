b2686db92cfcf7a586bfc584977bd7dfc779dbf8
Pedro Escapalete <pedroescapalete@gmail.com>
2025-04-15 21:07:24 -0300

    adiciona classificação de clima por temperatura

---
+++ if_Else/ex09.js
@@ -0,0 +1,8 @@
+let clima = 1;
+
+if (clima <= 15){
+    console.log(`Está Frio!!`);
+} else if(clima <= 25){
+    console.log(`O clima está agradavél`);
+} else 
+    console.log(`O calor do Cão!!!`)
